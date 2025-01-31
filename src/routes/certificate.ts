92480fba8669c9551d192e2bffb909bd9f1a8054
mongtrithay <trithay.mong@institute.pse.ngo>
2025-01-31 09:03:28 +0700

    update more

--- src/routes/certificate.ts
+++ src/routes/certificate.ts
@@ -1,10 +1,11 @@
 import protectRoute from "../middleware/auth";
 import { Router } from "express";
 import { certificate, getCertificate, getCertificateById, delCertificateById } from "../controllers/certificate.controller";
+import { RoleEnum } from "../common";
 
 const router = Router();
 
-router.post("/create", protectRoute(), certificate);
+router.post("/create", protectRoute(RoleEnum), certificate);
 router.get("/userId", protectRoute(), getCertificate);
 router.get("/userId/:id", protectRoute(), getCertificateById);
 router.delete("/userId/:id", protectRoute(), delCertificateById);
